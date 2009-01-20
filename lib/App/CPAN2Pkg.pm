273574ca93d3fef595945dbead21586f56f3bb13
Jérôme Quelin <jquelin@gmail.com>
2009-01-20 14:01:35 +0100

    pod update

--- lib/App/CPAN2Pkg.pm
+++ lib/App/CPAN2Pkg.pm
@@ -59,76 +59,71 @@ __END__
 
 =head1 NAME
 
-App::CPAN2Pkg - The great new App::CPAN2Pkg!
+App::CPAN2Pkg - generating native linux packages from cpan
 
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
 
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use App::CPAN2Pkg;
-
-    my $foo = App::CPAN2Pkg->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Jerome Quelin, C<< <jquelin at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-app-cpan2pkg at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-CPAN2Pkg>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+    $ cpan2pkg
+    $ cpan2pkg Module::Foo Module::Bar ...
 
 
 
+=head1 DESCRIPTION
 
-=head1 SUPPORT
+Don't use this module directly, refer to the C<cpan2pkg> script instead.
 
-You can find documentation for this module with the perldoc command.
+C<App::CPAN2Pkg> is the controller for the C<cpan2pkg> application. It
+implements a POE session, responsible to schedule and advance module
+packagement.
 
-    perldoc App::CPAN2Pkg
+It is spawned by the poe session responsible for the user interface.
 
 
-You can also look for information at:
+
+=head1 PUBLIC PACKAGE METHODS
+
+=head2 my $id = App::CPAN2Pkg->spawn( \%params )
+
+This method will create a POE session responsible for coordinating the
+package(s) creation.
+
+It will return the POE id of the session newly created.
+
+You can tune the session by passing some arguments as a hash
+reference, where the hash keys are:
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item * modules => \@list_of_modules
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-CPAN2Pkg>
+A list of modules to start packaging.
+
+
+=back
+
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<app-cpan2pkg at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-CPAN2Pkg>. I will
+be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+
+
+=head1 SEE ALSO
+
+Our git repository is located at L<git://repo.or.cz/app-cpan2pkg.git>,
+and can be browsed at L<http://repo.or.cz/w/app-cpan2pkg.git>.
+
+
+You can also look for information on this module at:
+
+=over 4
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
@@ -138,24 +133,26 @@ L<http://annocpan.org/dist/App-CPAN2Pkg>
 
 L<http://cpanratings.perl.org/d/App-CPAN2Pkg>
 
-=item * Search CPAN
+=item * Open bugs
 
-L<http://search.cpan.org/dist/App-CPAN2Pkg>
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-CPAN2Pkg>
 
 =back
 
 
-=head1 ACKNOWLEDGEMENTS
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin@cpan.org> >>
+
 
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Jerome Quelin, all rights reserved.
+Copyright (c) 2009 Jerome Quelin, all rights reserved.
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
 
-1; # End of App::CPAN2Pkg
