cccc7c9ec1d522e6f66072bcfcd5bfa8f1a6f56e
Jérôme Quelin <jquelin@gmail.com>
2009-01-20 14:14:49 +0100

    pod update

--- lib/App/CPAN2Pkg/Curses.pm
+++ lib/App/CPAN2Pkg/Curses.pm
@@ -57,34 +57,29 @@ sub _start {
 #
 
 
+#    CPAN2Mdv - generating mandriva rpms from cpan
+#
+#    - Packages queue ------------------------------------------
+#    Language::Befunge [ok]
+#        Test::Exception [ok]
+#            Test::Harness [ok]
+#                File::Spec [ok]
+#                    Scalar::Util [ok]
+#                    Carp [ok]
+#                    Module::Build [ok]
+#                        Test::More [ok]
+#                    ExtUtils::CBuilder [ok]
+#            Sub::Uplevel [ok]
+#        Storable [ok]
+#        aliased [ok]
+#        Readonly [ok]
+#        Class::XSAccessor [ok]
+#            AutoXS::Header [ok]
+#        Math::BaseCalc [ok]
+#        UNIVERSAL::require [ok]
+#
+#    enter = jump to, n = new, d = delete
 
-=pod
-
-
-CPAN2Mdv - generating mandriva rpms from cpan
-
-- Packages queue ------------------------------------------
-Language::Befunge [ok]
-    Test::Exception [ok]
-        Test::Harness [ok]
-            File::Spec [ok]
-                Scalar::Util [ok]
-                Carp [ok]
-                Module::Build [ok]
-                    Test::More [ok]
-                ExtUtils::CBuilder [ok]
-        Sub::Uplevel [ok]
-    Storable [ok]
-    aliased [ok]
-    Readonly [ok]
-    Class::XSAccessor [ok]
-        AutoXS::Header [ok]
-    Math::BaseCalc [ok]
-    UNIVERSAL::require [ok]
-
-enter = jump to, n = new, d = delete
-
-=cut
 
 #--
 # METHODS
@@ -135,3 +130,68 @@ sub _set_bindings {
 
 1;
 __END__
+
+
+=head1 NAME
+
+App::CPAN2Pkg::Curses - curses user interface for cpan2pkg
+
+
+
+=head1 DESCRIPTION
+
+C<App::CPAN2Pkg::Curses> implements a POE session driving a curses
+interface for C<cpan2pkg>.
+
+It is spawned directly by C<cpan2pkg> (since C<Curses::UI::POE> is a bit
+special regarding the event loop), and is responsible for launching the
+application controller (see C<App::CPAN2Pkg>).
+
+
+
+=head1 PUBLIC PACKAGE METHODS
+
+=head2 my $cui = App::CPAN2Pkg->spawn( \%params )
+
+This method will create a POE session responsible for creating the
+curses UI and reacting to it.
+
+It will return a C<App::CPAN2Pkg::Curses> object, which inherits from
+C<Curses::UI::POE>.
+
+You can tune the session by passing some arguments as a hash
+reference, where the hash keys are:
+
+=over 4
+
+=item * modules => \@list_of_modules
+
+A list of modules to start packaging.
+
+
+=back
+
+
+
+=head1 SEE ALSO
+
+For all related information (bug reporting, source code repository,
+etc.), refer to C<App::CPAN2Pkg>'s pod, section C<SEE ALSO>.
+
+
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin@cpan.org> >>
+
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2009 Jerome Quelin, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
