3acb99d5e488a4b74beccfaa50d70ec3db4c2cc9
Jérôme Quelin <jquelin@gmail.com>
2011-05-09 14:37:54 +0200

    new variable: $LINUX_FLAVOUR

--- lib/App/CPAN2Pkg/Utils.pm
+++ lib/App/CPAN2Pkg/Utils.pm
@@ -5,16 +5,18 @@ use warnings;
 package App::CPAN2Pkg::Utils;
 # ABSTRACT: various utilities for cpan2pkg
 
+use Devel::Platform::Info::Linux;
 use Exporter::Lite;
 use File::ShareDir::PathClass qw{ dist_dir };
 use FindBin                   qw{ $Bin };
 use Path::Class;
  
-our @EXPORT_OK = qw{ $SHAREDIR };
+our @EXPORT_OK = qw{ $LINUX_FLAVOUR $SHAREDIR };
 
 my $root = dir($Bin)->parent;
 our $IS_DEVEL  = -e $root->file("dist.ini" );
 our $SHAREDIR  = $IS_DEVEL ? $root->subdir("share") : dist_dir("App-CPAN2Pkg");
+our $LINUX_FLAVOUR = Devel::Platform::Info::Linux->new->get_info->{oslabel};
 
 1;
 __END__
