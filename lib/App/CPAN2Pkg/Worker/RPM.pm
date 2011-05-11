8ad3f0d83b54ad465d8ec0f464ffa033e913bb60
Jérôme Quelin <jquelin@gmail.com>
2011-05-11 15:02:55 +0200

    rpm worker needs a lock for rpm transactions

--- lib/App/CPAN2Pkg/Worker/RPM.pm
+++ lib/App/CPAN2Pkg/Worker/RPM.pm
@@ -6,8 +6,32 @@ package App::CPAN2Pkg::Worker::RPM;
 # ABSTRACT: worker specialized in rpm distributions
 
 use Moose;
+use MooseX::Has::Sugar;
+
+use App::CPAN2Pkg::Lock;
 
 extends 'App::CPAN2Pkg::Worker';
+
+
+# -- attributes
+
+=attr rpmlock
+
+A lock (L<App::CPAN2Pkg::Lock> object) to prevent more than one rpm
+installation at a time. Note that this object is common to all workers.
+
+=cut
+
+has rpmlock => ( ro, isa=>'App::CPAN2Pkg::Lock', lazy_build );
+
+
+# -- initialization
+
+sub _build_rpmlock {
+    state $lock = App::CPAN2Pkg::Lock->new;
+    return $lock;
+}
+
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
