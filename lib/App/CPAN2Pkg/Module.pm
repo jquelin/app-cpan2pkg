f350c8f41d3b9b3d7790ab73c9b0fafe183c4d98
Jérôme Quelin <jquelin@gmail.com>
2011-05-13 14:14:45 +0200

    remove dead code

--- lib/App/CPAN2Pkg/Module.pm
+++ lib/App/CPAN2Pkg/Module.pm
@@ -192,38 +192,6 @@ sub import_upstream {
     $self->_wheel($wheel);
 }
 
-sub install_from_dist {
-    my ($k, $self) = @_[KERNEL, OBJECT];
-    my $name = $self->name;
-
-    # check whether there's another rpm transaction
-    if ( $rpm_locked ) {
-        $self->_log_prefixed_lines("waiting for rpm lock... (owned by $rpm_locked)");
-        $k->delay( install_from_dist => 1 );
-        return;
-    }
-    $rpm_locked = $name;
-
-    # preparing command
-    my $cmd  = "sudo urpmi --auto 'perl($name)'";
-    $self->_log_new_step('Installing from upstream', "Running command: $cmd" );
-
-    # running command
-    $self->_output('');
-    $ENV{LC_ALL} = 'C';
-    my $wheel = POE::Wheel::Run->new(
-        Program      => $cmd,
-        StdoutEvent  => '_stdout',
-        StderrEvent  => '_stderr',
-        Conduit      => 'pty-pipe', # urpmi wants a pty
-        StdoutFilter => POE::Filter::Line->new,
-        StderrFilter => POE::Filter::Line->new,
-    );
-    $k->sig( CHLD => '_install_from_dist' );
-
-    # need to store the wheel, otherwise the process goes woo!
-    $self->_wheel($wheel);
-}
 
 sub install_from_local {
     my ($k, $self) = @_[KERNEL, OBJECT];
@@ -256,27 +224,6 @@ sub install_from_local {
 
     # need to store the wheel, otherwise the process goes woo!
     $self->_wheel($wheel);
-}
-
-
-sub is_installed {
-    my ($k, $self) = @_[KERNEL, OBJECT];
-
-    my $name = $self->name;
-    my $cmd  = qq{ require $name };
-    $self->_log_new_step(
-        'Checking if module is installed',
-        "Evaluating command: $cmd"
-    );
-
-    eval $cmd;
-    my $what = $@ || "$name loaded successfully\n";
-    $k->post('ui', 'append', $self, $what);
-
-    my $is_installed = $@ eq '';
-    my $status = $is_installed ? 'installed' : 'not installed';
-    $self->_log_result("$name is $status locally.");
-    $k->post('app', 'local_status', $self, $is_installed);
 }
 
 # -- private events
@@ -366,53 +313,6 @@ sub _import_upstream {
 }
 
 
-sub _find_prereqs {
-    my ($k, $self, $id) = @_[KERNEL, OBJECT, ARG0];
-
-    # terminate wheel
-    my $wheel  = $self->_wheel;
-    $self->_wheel(undef);
-
-    # extract prereqs
-    my @lines   = split /\n/, $self->_output;
-    my @tabbed  = grep { s/^\s+// } @lines;
-    my $idx     = firstidx { /^Module\s+Req Ver.*Satisfied/ } @tabbed;
-    my @wanted  = @tabbed[ $idx+1 .. $#tabbed ];
-    my @prereqs = map  { (split /\s+/, $_)[0] } @wanted;
-
-    # store prereqs
-    $self->prereqs( \@prereqs );
-    my @logs = @prereqs
-        ? map { "prereq found: $_" } @prereqs
-        : 'No prereqs found.';
-    $self->_log_result(@logs);
-    $k->post('app', 'prereqs', $self, @prereqs);
-}
-
-sub _install_from_dist {
-    my($k, $self, $pid, $rv) = @_[KERNEL, OBJECT, ARG1, ARG2];
-
-    # since it's a sigchld handler, it also gets called for other
-    # spawned processes. therefore, screen out processes that are
-    # not related to this object.
-    return unless defined $self->_wheel;
-    return unless $self->_wheel->PID == $pid;
-
-    # terminate wheel
-    $self->_wheel(undef);
-
-    # release rpm lock
-    $rpm_locked = '';
-
-    # log result
-    my $name  = $self->name;
-    my $exval = $rv >> 8;
-    my $status = $exval ? 'not been' : 'been';
-    $self->_log_result( "$name has $status installed from upstream." );
-    $k->post('app', 'upstream_install', $self, !$exval);
-}
-
-
 sub _install_from_local {
     my($k, $self, $pid, $rv) = @_[KERNEL, OBJECT, ARG1, ARG2];
 
@@ -437,77 +337,6 @@ sub _install_from_local {
     $k->post('app', 'local_install', $self, !$exval);
 }
 
-
-
-#--
-# METHODS
-
-# -- public methods
-
-sub blocking_add {
-    my ($self, $name) = @_;
-    $self->_blocking->{$name} = 1;
-}
-
-sub blocking_clear {
-    my ($self, $name) = @_;
-    $self->_blocking({});
-}
-
-sub blocking_list {
-    my ($self) = @_;
-    my $blocking = $self->_blocking;
-    return sort keys %$blocking;
-}
-
-#
-
-sub missing_add {
-    my ($self, $name) = @_;
-    warn "$self / $name";
-    $self->_missing->{$name} = 1;
-}
-
-sub missing_del {
-    my ($self, $name) = @_;
-    delete $self->_missing->{$name};
-}
-
-sub missing_list {
-    my ($self) = @_;
-    my $missing = $self->_missing;
-    return sort keys %$missing;
-}
-
-# -- private methods
-
-sub _log_empty_line {
-    my ($self, $nb) = @_;
-    $nb //= 1; #/ FIXME padre syntaxic color glitch
-    POE::Kernel->post('ui', 'append', $self, "\n" x $nb);
-}
-
-sub _log_prefixed_lines {
-    my ($self, @lines) = @_;
-
-    my $prefix = '*';
-    POE::Kernel->post('ui', 'append', $self, $_)
-        for map { "$prefix $_\n" } @lines;
-}
-
-sub _log_new_step {
-    my ($self, $step, $comment) = @_;
-
-    $self->_log_prefixed_lines('-' x 10, $step, '', $comment, '');
-    $self->_log_empty_line;
-}
-
-sub _log_result {
-    my ($self, @lines) = @_;
-
-    $self->_log_empty_line;
-    $self->_log_prefixed_lines( '', @lines, '', '' );
-}
 
 
 1;
