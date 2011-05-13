b79ab016537e8ffc7d11e95af517e7d7798c8a6d
Jérôme Quelin <jquelin@gmail.com>
2011-05-13 14:31:44 +0200

    storing prereqs in app::cpan2pkg::module

--- lib/App/CPAN2Pkg/Module.pm
+++ lib/App/CPAN2Pkg/Module.pm
@@ -27,25 +27,43 @@ The L<App::CPAN2Pkg::Repository> for the local system.
 
 The L<App::CPAN2Pkg::Repository> for the upstream Linux distribution.
 
+=attr prereqs
+
+The modules listed as prerequesites for the module. Note that each
+repository (local & upstream) keep in addition a list of prereqs that
+are B<missing>. This list keeps the module prereqs, even after they have
+been fulfilled on the repositories.
+
 =cut
 
 has name => ( ro, required, isa=>'Str' );
 has local    => ( ro, isa=>"App::CPAN2Pkg::Repository", default=>sub{ App::CPAN2Pkg::Repository->new } );
 has upstream => ( ro, isa=>"App::CPAN2Pkg::Repository", default=>sub{ App::CPAN2Pkg::Repository->new } );
 
+has prereqs => (
+    ro, auto_deref,
+    isa     => 'ArrayRef[Str]',
+    traits  => ['Array'],
+    default => sub { [] },
+    handles => {
+        _add_prereq => 'push',
+    },
+);
 
 # -- public methods
 
 =method add_prereq
 
-    $module->add_prereq( );
+    $module->add_prereq( $modname );
 
-Add a prerequesite to the module on both local & upstream repositories.
+Add a prerequesite to the module, also list it as missing on both local
+& upstream repositories.
 
 =cut
 
 sub add_prereq {
     my ($self, $p) = @_;
+    $self->_add_prereq($p);
     $self->local->add_prereq($p);
     $self->upstream->add_prereq($p);
 }
@@ -58,7 +76,6 @@ use Class::XSAccessor
         is_avail_on_bs => 'is_avail_on_bs',
         is_local       => 'is_local',  # if module is available locally
         _name__           => 'name',
-        prereqs        => 'prereqs',
         # private
         _blocking  => '_blocking',
         _missing   => '_missing',
